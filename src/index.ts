f6e994700a443d5d8b52b0aaecdd9f0a4ebad685
Moovendhan <vmoovendan3@gmail.com>
2024-11-26 08:07:49 +0530

    Adding commands in index file

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 // index.ts
 
 import InsertUserCommand from "@/commands/user/CreateUserCommand";
+import GetUsersCommand from "@/commands/user/GetUsersCommand";
+import CreateGroupCommand from "@/commands/group/CreateGroupCommand";
+import InsertChannelCommand from "@/commands/channel/InsertChannleCommand";
+
 console.log('Running index file');
 console.log("testing");
 
-async function createUser() {
-  // Insert a new user
-  const insertUserCommand = new InsertUserCommand({username: "Moovendhan", email: "vmoovendhan3@gmail.com"});
-  const newUser = await insertUserCommand.execute();
-  console.log('Inserted User:', newUser);
-}
-createUser();
-  
+// async function createUser() {
+//   // Insert a new user
+//   const insertUserCommand = new InsertUserCommand({username: "Moovendhan", email: "vmoovendhan3@gmail.com"});
+//   const newUser = await insertUserCommand.execute();
+//   console.log('Inserted User:', newUser);
+// }
+// createUser();
+
 
 // async function gerUser() {
 //   const getUsersCommand = new GetUsersCommand();
@@ -21,11 +25,8 @@ createUser();
 // gerUser();
 
 // async function createGroups() {
-//     const adminIds = ['66f8e98a9b1ee76d34240d81'];
-//     const memberIds = ['66d0c154c5fc131fb72044ae'];
-  
-//     const createGroupsCommand = new CreateGroupCommand('My testsss', adminIds, memberIds);
-  
+//     const createGroupsCommand = new CreateGroupCommand({ name: "Group name", adminIds: ["67452ef0510ad81dc435d777"], memberIds: ["67452ef0510ad81dc435d777"] });
+
 //     try {
 //       const group = await createGroupsCommand.execute();
 //       console.log('Group created successfully:', group);
@@ -33,18 +34,26 @@ createUser();
 //       console.error('Error creating group:', error);
 //     }
 //   }
-  
+
 // createGroups();
 
 // async function createChannle() {
-//   const insertChannelCommand = new InsertChannelCommand("testechannle", "teste desc", '66f8e98a9b1ee76d34240d83', true);
+//     const insertChannelCommand = new InsertChannelCommand();
 
-//   try {
-//     const channel = await insertChannelCommand.execute();
-//     console.log('Channel created:', channel);
-//   } catch (error) {
-//     console.error('Failed to create channel:', error);
-//   }
+//     const input = {
+//         name: "Chnnelname",
+//         description: "descriptions",
+//         userId: "67452ef0510ad81dc435d777",
+//         isAdmin: true
+//       }
+
+      
+//     try {
+//         const channel = await insertChannelCommand.createChannel(input);
+//         console.log('Channel created:', channel);
+//     } catch (error) {
+//         console.error('Failed to create channel:', error);
+//     }
 // }
 // createChannle();
 
