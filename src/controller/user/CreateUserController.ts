7bb95b013545897edc74e4f3cf7eb8d98386ae89
Moovendhan <vmoovendan3@gmail.com>
2024-12-10 21:39:43 +0530

    Adding the prisma error handlers

--- src/controller/user/CreateUserController.ts
+++ src/controller/user/CreateUserController.ts
@@ -1,21 +1,28 @@
 import { Request, Response } from 'express';
 import CreateUserService from '@/service/user/CreateUserService';
 import { CreateUserInput } from "@/types/CreateUser";
+import { handlePrismaError } from "@/utils/error_handler/prismaErrorHandler";
 
 class CreateUsersController {
     private service: CreateUserService = new CreateUserService();
 
-    createUser = async (req: Request, res: Response): Promise<void> => {
+    createUser = async (req: Request, res: Response) => {
         try {
 
             const body: CreateUserInput = req.body;
             console.log('body', body)
             const users = await this.service.createUser(body);
 
-            res.status(200).json(users);
+            return res.status(200).json(users);
 
         } catch (error: any) {
             console.error(error);
+
+            const prismaError = handlePrismaError(error);
+            if (prismaError) {
+                return res.status(prismaError.statusCode).json(prismaError);
+            }
+
             res.status(500).json({ error: "Internal server error" });
         }
     };
