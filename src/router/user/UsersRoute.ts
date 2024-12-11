a9faafaa91b9d232c0ddbfb33a250cd28ee3efe3
Moovendhan <vmoovendan3@gmail.com>
2024-12-11 22:34:52 +0530

    Adding permission for the userrole

--- src/router/user/UsersRoute.ts
+++ src/router/user/UsersRoute.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import GetUsersController from '@/controller/user/GetUsersController';
 import CreateUsersController from '@/controller/user/CreateUserController';
 import { queryParamMiddleware } from '@/middleware/queryParamsMiddleware';
+import { bodyFieldValidationMiddleware } from "@/middleware/bodyParamsMiddleware";
 
 const userRouter = Router();
 const GetUserController = new GetUsersController();
@@ -11,6 +12,6 @@ userRouter.get('/all', queryParamMiddleware, GetUserController.getAllUsers);
 
 userRouter.get('/', queryParamMiddleware, GetUserController.getUserDetails);
 
-userRouter.post('/', CreateUserController.createUser);
+userRouter.post('/', bodyFieldValidationMiddleware, CreateUserController.createUser);
 
 export default userRouter;
